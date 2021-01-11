d4a33dfd10a5a791ab51351d458d6895eb8a5952
Rafael Calunga <rafaelcalunga@gmail.com>
2021-01-10 23:56:28 -0500

    Generating and scaling up a QR code

--- HotProspects/MeView.swift
+++ HotProspects/MeView.swift
@@ -6,10 +6,52 @@
 //
 
 import SwiftUI
+import CoreImage.CIFilterBuiltins
 
 struct MeView: View {
+    @State private var name = "Anounymous"
+    @State private var emailAddress = "you@yoursite.com"
+    
+    let context = CIContext()
+    let filter = CIFilter.qrCodeGenerator()
+    
     var body: some View {
-        Text("Me")
+        NavigationView {
+            VStack {
+                TextField("Name", text: $name)
+                    .textContentType(.name)
+                    .font(.title)
+                    .padding(.horizontal)
+                
+                TextField("Email address", text: $emailAddress)
+                    .textContentType(.emailAddress)
+                    .font(.title)
+                    .padding([.horizontal, .bottom])
+                
+                Image(uiImage: generateQRCOde(from: "\(name)\n\(emailAddress)"))
+                    .interpolation(.none)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200, height: 200)
+                
+                Spacer()
+            }
+            .navigationTitle("Your code")
+        }
+    }
+    
+    
+    func generateQRCOde(from string: String) -> UIImage {
+        let data  = Data(string.utf8)
+        filter.setValue(data, forKey: "inputMessage")
+        
+        if let outputImage = filter.outputImage {
+            if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
+                return UIImage(cgImage: cgimg)
+            }
+        }
+        
+        return UIImage(systemName: "xmark.circle") ?? UIImage()
     }
 }
 
